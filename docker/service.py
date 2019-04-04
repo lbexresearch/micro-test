56c8238ba6676e18b911d1b4c67cb4eb6b5f1e14
Magnus Byström <magnus.bystrom@softronic.se>
2019-04-04 14:18:44 +0200

    Added sleep, crash and version

--- docker/service.py
+++ docker/service.py
@@ -1,4 +1,5 @@
 import hug
+import sys
 import platform
 import time
 from random import randint
@@ -14,17 +15,43 @@ def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
     return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
             'took': float(hug_timer)}
 
-@hug.get('/uptime', versions=1)
+@hug.get('/sleep', examples='sec=2',versions=[1,2])
+@hug.local()
+def sleep(sec: hug.types.float_number):
+    time.sleep(sec)
+    return {'sleep': True, 'took': sec}
+
+@hug.get('/', versions=1)
 def root():
+    return {'micro-test': "0.0.1", 'took': 0}
+
+@hug.get('/uptime', versions=1)
+@hug.local()
+def uptime():
     return {'uptime': time.time() - starttime, 'took': 0}
 
 @hug.get('/uptime', versions=2)
-def root():
+@hug.local()
+def uptime():
     t=randint(0,9) 
-    time.sleep(t)
-    return {'uptime': time.time() - starttime, 'took': t}
+    time.sleep(t/10)
+    return {'uptime': time.time() - starttime, 'took': t/10}
 
-
-@hug.get('/platform')
-def root():
+@hug.get('/platform', versions=[1,2])
+@hug.local()
+def platform():
     return {'platform': platform.platform(), 'took': 0}
+
+@hug.get('/version', versions=range(1,3))
+@hug.local()
+def version():
+    return {'version': 3, 'took': 0}
+
+
+
+@hug.get('/crash', versions=[1,2])
+@hug.local()
+def platform():
+    sys.exit(1)
+    return {'crash': "Now", 'took': 0}
+
